36907937ba8642a57b96bcf93d1e311e6d05a0ba
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2021-11-25 10:53:17 +0000

    Create CI workflow

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,8 @@
+import {render} from '@testing-library/react';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import Providers from "../Providers";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders', () => {
+  render(<Providers><App /></Providers>);
 });
